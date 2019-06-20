f941b1608328a5b2edb13a6b4e81486f76ada256
Richard <richard.anaya@gmail.com>
2019-06-19 22:52:02 -0700

    renamings

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,34 @@ fn run_window(title: &str, width: f64, height: f64) {
     let mut event_loop = glutin::EventsLoop::new();
 
     // Define the window we want to build
-    let wb = glutin::WindowBuilder::new()
+    let window_builder = glutin::WindowBuilder::new()
         .with_title(&title.to_string())
         .with_dimensions(glutin::dpi::LogicalSize::new(width,height));
     
     // Define the opengl we want and then build it
-    let windowed_context = glutin::ContextBuilder::new()
+    let gl_context = glutin::ContextBuilder::new()
         .with_gl(glutin::GlRequest::GlThenGles {
                     opengl_version: (3, 2),
                     opengles_version: (3, 0),
         })
-        .build_windowed(wb, &event_loop).unwrap();
+        .build_windowed(window_builder, &event_loop).unwrap();
     
     // Take the opengl context
-    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    let gl_context = unsafe { gl_context.make_current().unwrap() };
     
     // Get a handle to opengl
-    let gl = match windowed_context.get_api() {
+    let gl = match gl_context.get_api() {
             glutin::Api::OpenGl => unsafe {
-                gl::GlFns::load_with(|symbol| windowed_context.get_proc_address(symbol) as *const _)
+                gl::GlFns::load_with(|symbol| gl_context.get_proc_address(symbol) as *const _)
             },
             glutin::Api::OpenGlEs => unsafe {
-                gl::GlesFns::load_with(|symbol| windowed_context.get_proc_address(symbol) as *const _)
+                gl::GlesFns::load_with(|symbol| gl_context.get_proc_address(symbol) as *const _)
             },
             glutin::Api::WebGl => unimplemented!(),
     };
 
     // Get a DPI factor
-    let _device_pixel_ratio = windowed_context.window().get_hidpi_factor() as f32;
+    let _device_pixel_ratio = gl_context.window().get_hidpi_factor() as f32;
     
     // Render until closed
      event_loop.run_forever(|event| {
@@ -48,7 +48,7 @@ fn run_window(title: &str, width: f64, height: f64) {
             _ => {
                 gl.clear_color(1.0,0.0,0.0,1.0);
                 gl.clear(gl::COLOR_BUFFER_BIT);
-                windowed_context.swap_buffers().ok();
+                gl_context.swap_buffers().ok();
                 ControlFlow::Continue
             },
         }
